fa0579a6f0f401ea7849ea5b7b0b3e39da65b52e
bongbong4043 <bong4043@paran.com>
2023-12-08 13:50:05 +0900

    add java maven spring boot (web) skeleton

---
+++ src/main/java/com/gw/java_mvn_spw/JavaMvnSpwApplication.java
@@ -0,0 +1,13 @@
+package com.gw.java_mvn_spw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvnSpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMvnSpwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gw/java_mvn_spw/JavaMvnSpwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gw.java_mvn_spw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvnSpwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
