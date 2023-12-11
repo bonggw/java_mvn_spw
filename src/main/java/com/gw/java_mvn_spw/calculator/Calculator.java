77afcbc4715ec650fc64060e5adaeeba5ddab0f1
bongbong4043 <bong4043@paran.com>
2023-12-11 13:37:17 +0900

    * google java sytel로 소스 변경

--- src/main/java/com/gw/java_mvn_spw/calculator/Calculator.java
+++ src/main/java/com/gw/java_mvn_spw/calculator/Calculator.java
@@ -1,19 +1,22 @@
 package com.gw.java_mvn_spw.calculator;
-import org.springframework.stereotype.Service;
 
 import java.util.stream.DoubleStream;
+import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-    public int add(int a, int b) {
-        System.out.println("call add(..)");
-        return a + b;
-    }
 
-
-    static double multiply(double... operands) {
-        System.out.println("call multiply(..)");
-        return DoubleStream.of(operands)
-                .reduce(1, (a, b) -> a * b);
+  public int add(int a, int b) {
+    System.out.println("call add(..)");
+    if (a < 0 || b < 0) {
+      a += 10;
     }
+    return a + b;
+  }
+
+  static double multiply(double... operands) {
+    System.out.println("call multiply(..)");
+    return DoubleStream.of(operands)
+      .reduce(1, (a, b) -> a * b);
+  }
 }
